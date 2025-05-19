e31b8aece484b4ffb7157f35e2eae1313e211523
鍾承翰 <hcchung3@icloud.com>
2025-05-19 23:47:27 +0800

    Update

--- DQN.py
+++ DQN.py
@@ -341,19 +341,19 @@ if __name__ == "__main__":
     # training section:
 
 
-    # for i in range(1):
-    #     time0 = time.time()
-    #     print(f"#{i + 1} training progress")
-    #     train(env, 200)
-    #     time1 = time.time()
-    #     print(f"Training time: {time1 - time0} seconds")
-    #     print ("Win rate: ", env.win_count ,"/", env.win_count + env.dead_count, f"({env.get_win_rate()})")
-    #     [profit, loss] = env.get_cumulative_profit_loss_ratio()
-    #     print("Profit Loss Ratio: ",f"{profit} : {loss}" )
-    #     print ("Final profit rate: ", env.get_profit_rate())
+    for i in range(1):
+        time0 = time.time()
+        print(f"#{i + 1} training progress")
+        train(env, 200)
+        time1 = time.time()
+        print(f"Training time: {time1 - time0} seconds")
+        print ("Win rate: ", env.win_count ,"/", env.win_count + env.dead_count, f"({env.get_win_rate()})")
+        [profit, loss] = env.get_cumulative_profit_loss_ratio()
+        print("Profit Loss Ratio: ",f"{profit} : {loss}" )
+        print ("Final profit rate: ", env.get_profit_rate())
 
 
-    # # testing section:
-    # test(env, "./Tables/DQN_GG.pt")
-    # env.close()
+    # testing section:
+    test(env, "./Tables/DQN_GG.pt")
+    env.close()
 
